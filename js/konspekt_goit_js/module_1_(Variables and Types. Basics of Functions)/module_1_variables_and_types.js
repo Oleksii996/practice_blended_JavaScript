d1378fe9d42fef3ee1cda809e6b7ac09783cab26
oleksii ponomarenko <alexponomarenko996@gmail.com>
2025-09-30 15:29:09 +0200

    update code

---
+++ js/konspekt_goit_js/module_1_(Variables and Types. Basics of Functions)/module_1_variables_and_types.js
@@ -0,0 +1,73 @@
+//#region Оголошення змінної (Declaration a Value)
+{
+  let price = 20;
+  console.log(price); // 20
+}
+{
+  let age = 20;
+  console.log(age); // 20
+}
+{
+  const username = "Mango";
+  console.log(username); // "Mango"
+}
+//#endregion
+
+//#region Перевизначення змінної (Reassigning a Value)
+{
+  let username = "Mango";
+  username = "Poly";
+  console.log(username); // "Poly"
+}
+{
+  let productPrice = "1500EUR";
+  productPrice = "1800USD";
+  console.log(productPrice); // "Poly"
+}
+//#endregion
+
+//#region Типи даних (Data Types)
+// Number
+{
+  const age = 20;
+  const salary = 3710.84;
+}
+// Number
+{
+  const username = "Mango995";
+  const description = "JavaScript is awesome!";
+}
+// Number
+{
+  const isModalOpen = true;
+  const isLoggedIn = false;
+}
+//#endregion
+
+//#region Math operations
+{
+  const x = 8;
+  const y = 5;
+  console.log(x - y); // 3
+}
+{
+  const x = 8;
+  const y = 5;
+  console.log(x + y); // 13
+}
+{
+  const x = 8;
+  const y = 5;
+  console.log(x * y); // 40
+}
+{
+  const x = 8;
+  const y = 5;
+  console.log(x / y); // 1.6
+}
+{
+  const x = 8;
+  const y = 5;
+  console.log(x ** y); // 32768
+}
+//#endregion
