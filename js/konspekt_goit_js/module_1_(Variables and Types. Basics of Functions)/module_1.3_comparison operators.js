4c1fa961f337fee70c78b8b4b0e01c89974e2f70
oleksii ponomarenko <alexponomarenko996@gmail.com>
2025-10-01 14:28:37 +0200

    udpate code

--- js/konspekt_goit_js/module_1_(Variables and Types. Basics of Functions)/module_1.3_comparison operators.js
+++ js/konspekt_goit_js/module_1_(Variables and Types. Basics of Functions)/module_1.3_comparison operators.js
@@ -49,9 +49,8 @@ console.log("5" * 2); // 10
 console.log("10" - 5); // 5
 console.log(5 + true); // 6
 console.log(5 - true); // 4
-//#endregion
 
-//#region Перетворення рядків у числа (String to Number Conversion)
+//Перетворення рядків у числа (String to Number Conversion)
 //Метод Number.parseInt()
 console.log(Number.parseInt("5")); // 5
 console.log(Number.parseInt("5.5")); // 5
@@ -61,7 +60,6 @@ console.log(Number.parseInt("12.46qwe79")); // 12
 console.log(Number.parseInt("cm5")); // NaN
 console.log(Number.parseInt("")); // NaN
 console.log(Number.parseInt("qweqwe")); // NaN;
-
 //Метод Number.parseFloat()
 console.log(Number.parseFloat("5")); // 5
 console.log(Number.parseFloat("5.5")); // 5.5
@@ -73,4 +71,39 @@ console.log(Number.parseFloat("12.46qwe79")); // 12.46
 console.log(Number.parseFloat("cm5")); // NaN
 console.log(Number.parseFloat("")); // NaN
 console.log(Number.parseFloat("qweqwe")); // NaN
+//#endregion
+
+//#region Арифметичні функції (Arithmetic Functions)
+//Math.floor(num) - округлення до меншого цілого
+console.log(Math.floor(5.1)); // 5
+console.log(Math.floor(5.9)); // 5
+console.log(Math.floor(-5.1)); // -6
+console.log(Math.floor(-5.9)); // -6
+//Math.ceil(num) - округлення до більшого цілого
+console.log(Math.ceil(5.1)); // 6
+console.log(Math.ceil(5.9)); // 6
+console.log(Math.ceil(-5.1)); // -5
+console.log(Math.ceil(-5.9)); // -5
+//Math.round(num) - округлення до найближчого цілого
+console.log(Math.round(5.1)); // 5
+console.log(Math.round(5.5)); // 6
+console.log(Math.round(5.9)); // 6
+console.log(Math.round(-5.1)); // -5
+//Math.max(a, b, c, ...) - пошук найбільшого числа
+console.log(Math.max(1, 2, 3, 4, 5)); // 5
+console.log(Math.max(-1, -2, -3, -4, -5)); // -1
+//Math.min(a, b, c, ...) - пошук найменшого числа
+console.log(Math.min(1, 2, 3, 4, 5)); // 1
+console.log(Math.min(-1, -2, -3, -4, -5)); // -5
+//Math.random() - генерація псевдовипадкового числа від 0 до 1
+console.log(Math.random()); // 0.123456789...
+console.log(Math.random()); // 0.987654321...
+//#endregion
+
+//#region Дробові числа (Floating Point Numbers)
+console.log(0.1 + 0.2); // 0.30000000000000004
+console.log(0.1 + 0.2 === 0.3); // false
+//Метод toFixed(n) - повертає рядок з округленим числом до n знаків після коми
+console.log((0.1 + 0.2).toFixed(1)); // "0.3"
+console.log((0.1 + 0.2).toFixed(2)); // "0.30"
 //#endregion
