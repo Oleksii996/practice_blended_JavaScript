27e0e058443a471e76918b32020f695b67bf143e
oleksii ponomarenko <alexponomarenko996@gmail.com>
2025-10-09 21:46:45 +0200

    udpate code

--- js/konspekt_goit_js/module_3_(Arrays and Functions)/module_3.2_array methods.js
+++ js/konspekt_goit_js/module_3_(Arrays and Functions)/module_3.2_array methods.js
@@ -19,5 +19,51 @@
 
   console.log(transformString("user_age")); // "user-age"
   console.log(transformString("price_per_droid")); // "price-per-droid"
+  console.log(transformString("user_is_not_admin")); // "user-is-not-admin"
 }
+
+//Приклад задача
+//Функція getLength(array) очікує один параметр array - масив довільних значень. Доповни код функції так, щоб вона перетворювала масив у рядок, без роздільників, і повертала кількість символів в отриманому рядку.
+{
+  function getLength(array) {
+    const string = array.join("");
+    return string.length - 1;
+  }
+
+  console.log(getLength(["Mango", "hurries", "to", "the", "train"])); //21
+  console.log(getLength(["M", "a", "n", "g", "o"])); //4
+  console.log(getLength(["top", "picks", "for", "you"])); //13
+}
+{
+  function getLength(array) {
+    //array-параметр функції
+    const string = array.join(""); //string-назва змінної
+    return string;
+  }
+
+  console.log(getLength(["Mango", "hurries", "to", "the", "train"])); //Mangohurriestothetrain
+  console.log(getLength(["M", "a", "n", "g", "o"])); //Mango
+  console.log(getLength(["top", "picks", "for", "you"])); //toppicksforyou
+}
+
 //#endregion
+
+//#region Метод split()
+// Метод рядків split(delimiter) виконує зворотну операцію порівняно з методом масивів join(delimiter). Він дозволяє перетворити рядок на масив, розбивши його за вказаним роздільником delimiter.
+{
+  const name = "Mango";
+  const letters = name.split("");
+  console.log(letters); // ["M", "a", "n", "g", "o"]
+
+  const message = "JavaScript essentials";
+  const words = message.split(" ");
+  console.log(words); // ["JavaScript", "essentials"]
+
+  const slug = "amazing-french-recipes";
+  const slugParts = slug.split("-");
+  console.log(slugParts); // ["amazing", "french", "recipes"]
+}
+
+//#endregion
+
+//#region
