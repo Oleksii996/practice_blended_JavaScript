98468a2b35f4eddf182eaa0c78a43623e3904e2e
oleksii ponomarenko <alexponomarenko996@gmail.com>
2025-11-02 15:59:34 +0100

    update code

--- js/konspekt_goit_js/module_4_(Objects)/module_4.4_object methods.js
+++ js/konspekt_goit_js/module_4_(Objects)/module_4.4_object methods.js
@@ -187,5 +187,195 @@
 //#endregion
 
 //#regionМасив об’єктів
+// Ти вже знаєш, що об'єкт дає змогу згрупувати характеристики сутності, наприклад, книги.
+// Тому найчастіше будемо працювати з масивом об'єктів. Для цього зберігатимемо у властивості books не рядки, а об'єкти з назвою книги та рейтингом, а в майбутньому, можливо, й іншими характеристиками.
+{
+  const bookShelf = {
+    books: [
+      { title: "The Last Kingdom", rating: 8 },
+      { title: "The Mist", rating: 6 },
+    ],
+    getBooks() {
+      return this.books;
+    },
+  };
+}
 
+//Тепер метод getBooks повертатиме масив об'єктів. А метод addBook очікує в параметрі не рядок, а об'єкт книги і додає його в масив у властивості books.
+
+{
+  const bookShelf = {
+    books: [
+      { title: "The Last Kingdom", rating: 8 },
+      { title: "The Mist", rating: 6 },
+    ],
+    getBooks() {
+      return this.books;
+    },
+    addBook(newBook) {
+      this.books.push(newBook);
+    },
+  };
+
+  bookShelf.addBook({ title: "Dream Guardian", rating: 9 });
+  console.log(bookShelf.getBooks()); // [{ title: "The Last Kingdom", rating: 8 }, { title: "The Mist", rating: 6 }, { title: "Dream Guardian", rating: 9 }]
+}
+
+//При переборі масиву у властивості books треба пам'ятати, що це масив об'єктів. Наприклад, додамо метод getAverageRating(), який повертатиме середній рейтинг книг. Для цього:
+// Оголосимо новий метод getAvarageRating в об'єкті.
+// Оголосимо змінну totalRating для зберігання загального рейтингу.
+// Переберемо масив книг за посиланням this.books у циклі for...of.
+// На кожній ітерації додамо до загального рейтингу - рейтинг книги.
+// Після завершення циклу повернемо результат ділення загального рейтингу на кількість книг.
+
+{
+  const bookShelf = {
+    books: [
+      { title: "The Last Kingdom", rating: 8 },
+      { title: "The Mist", rating: 6 },
+    ],
+    getAvarageRating() {
+      let totalRating = 0;
+
+      for (const book of this.books) {
+        totalRating += book.rating;
+      }
+
+      return totalRating / this.books.length;
+    },
+  };
+
+  bookShelf.getAvarageRating();
+  console.log(bookShelf.getAvarageRating()); // 7
+}
+
+//Приклад задача
+// Замовниця хоче, щоб кожне зілля було представлено не тільки ім'ям, але й ціною. Тому зараз у властивості potions буде зберігатися масив об'єктів з властивостями name та price.
+
+// Об'єкт atTheOldToad має наступні властивості:
+// potions — масив рядків, з назвами зілль
+// getPotions() — метод, який повертає значення властивості potions
+// addPotion() — метод, який приймає параметр об'єкт нового зілля newPotion і додає його в кінець масиву у властивості potions.
+// Додай метод getTotalPrice(), який має повертати загальну вартість усіх зілль з властивості potions.
+
+{
+  const atTheOldToad = {
+    potions: [
+      { name: "Speed potion", price: 460 },
+      { name: "Stone skin", price: 520 },
+    ],
+    getPotions() {
+      return this.potions;
+    },
+    addPotion(newPotion) {
+      this.potions.push(newPotion);
+    },
+    getTotalPrice() {
+      let TotalPrice = 0;
+      for (const potion of this.potions) {
+        TotalPrice += potion.price;
+      }
+      return TotalPrice;
+    },
+  };
+
+  console.log(atTheOldToad.getPotions()); // ["Speed potion", "Stone skin"]
+  atTheOldToad.addPotion({ name: "Invisibility", price: 620 });
+  console.log(atTheOldToad.getPotions()); // ["Speed potion", "Stone skin","Invisibility"]
+  atTheOldToad.addPotion({ name: "Power potion", price: 270 });
+  console.log(atTheOldToad.getPotions()); // ["Speed potion", "Stone skin","Invisibility","Power potion"]
+  atTheOldToad.getTotalPrice();
+  console.log(atTheOldToad.getTotalPrice()); // 1870
+}
+
+//#endregion
+
+//#regionМасив Зміна об'єкта в масиві
+// Ціла група завдань зводиться до зміни значення властивості певного об'єкта в масиві. Наприклад, зміна рейтингу книги. Пошук необхідного об'єкта в масиві виконується за унікальним значенням властивості, наприклад, за ім'ям книги.
+
+{
+  const bookShelf = {
+    books: [
+      { title: "The Last Kingdom", rating: 8 },
+      { title: "The Mist", rating: 6 },
+    ],
+    changeRating(bookName, newRating) {},
+  };
+}
+
+// Метод changeRating очікує на назву книги, якій необхідно змінити рейтинг, і нове значення рейтингу, яке потрібно підмінити в об'єкті. Процес зміни властивостей об’єкта в масиві починається з таких кроків:
+// Перебір масиву об'єктів у циклі, наприклад for...of.
+// Додавання перевірки збігу значення властивості об'єкта на поточній ітерації і заданого значення.
+{
+  const bookShelf = {
+    books: [
+      { title: "The Last Kingdom", rating: 8 },
+      { title: "The Mist", rating: 6 },
+    ],
+    changeRating(bookName, newRating) {
+      for (const book of this.books) {
+        if (book.title === bookName) {
+          // Знайшли необхідний об’єкт за назвою книги
+        }
+      }
+    },
+  };
+}
+
+//При виконанні if ми можемо бути впевнені, що на даній ітерації в змінній book знаходиться посилання на необхідний нам об'єкт, оскільки об'єкти передаються за посиланнями. Тепер достатньо звернутися до властивості цього об'єкта і прирівняти йому нове значення.
+{
+  const bookShelf = {
+    books: [
+      { title: "The Last Kingdom", rating: 8 },
+      { title: "The Mist", rating: 6 },
+    ],
+    changeRating(bookName, newRating) {
+      for (const book of this.books) {
+        if (book.title === bookName) {
+          book.rating = newRating;
+        }
+      }
+    },
+  };
+
+  // ✅ Викликаємо метод через об’єкт
+  bookShelf.changeRating("The Mist", 9);
+  bookShelf.changeRating("The Last Kingdom", 4);
+
+  console.log(bookShelf.books); // [{ title: "The Last Kingdom", rating: 4 }, { title: "The Mist", rating: 9 }]
+}
+
+//Приклад-задача
+// Об'єкт atTheOldToad має наступні властивості:
+// potions — масив об'єктів зілль
+// getPotions() — метод, який повертає значення властивості potions
+// updatePotionName() — метод, який приймає два параметра рядків oldName і newName
+// Доповни метод updatePotionName(oldName, newName) таким чином, щоб він оновлював назву зілля з oldName на newName в масиві зілля у властивості potions.
+
+{
+  const atTheOldToad = {
+    potions: [
+      { name: "Speed potion", price: 460 },
+      { name: "Stone skin", price: 520 },
+    ],
+    getPotions() {
+      return this.potions;
+    },
+    updatePotionName(oldName, newName) {
+      for (const potion of this.potions) {
+        if (potion.name === oldName) {
+          potion.name = newName;
+        }
+      }
+    },
+  };
+
+  console.log(atTheOldToad.potions); // [{ name: 'Speed potion', price: 460 }, { name: 'Stone skin', price: 520 }]
+
+  atTheOldToad.updatePotionName("Stone skin", "Invisibility");
+  console.log(atTheOldToad.potions); // [{ name: 'Invisibility', price: 460 }, { name: 'Stone skin', price: 520 }]
+
+  atTheOldToad.updatePotionName("Speed potion", "Polymorth");
+  console.log(atTheOldToad.potions); // [{ name: 'Polymorth', price: 460 }, { name: 'Invisibility', price: 520 }]
+}
 //#endregion
