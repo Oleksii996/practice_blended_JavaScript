5360a69c67f819114fd8da0fd1d72e90757fc2c7
oleksii ponomarenko <alexponomarenko996@gmail.com>
2025-11-09 17:54:04 +0100

    udpate code

--- js/konspekt_goit_js/module_6_(Object-Oriented Programming - Classes)/module_6.1_execution context.js
+++ js/konspekt_goit_js/module_6_(Object-Oriented Programming - Classes)/module_6.1_execution context.js
@@ -126,23 +126,344 @@
   showThis(); // "this in showThis: undefined"
 }
 
-//Потім присвоюємо цю функцію властивості об'єкта і викликаємо її як метод цього об'єкта.
-("use strict");
+{
+  //Потім присвоюємо цю функцію властивості об'єкта і викликаємо її як метод цього об'єкта.
+  ("use strict");
 
-function showThis() {
-  console.log("this in showThis: ", this);
+  function showThis() {
+    console.log("this in showThis: ", this);
+  }
+
+  const user = {
+    username: "Poly",
+  };
+
+  user.showContext = showThis;
+
+  // Викликаємо в контексті об'єкта
+  user.showContext(); // this in showThis: {username: "Poly", showContext: ƒ}
+
+  // Викликаємо в глобальному контексті
+  showThis(); // "this in showThis: undefined"
 }
 
-const user = {
-  username: "Poly",
-};
+//#endregion
 
-user.showContext = showThis;
+//#region Метод call()
+// Бувають ситуації, коли функцію потрібно викликати в контексті об'єкта, навіть якщо функція не є методом цього об'єкта. Для цього у JavaScript існують спеціальні методи: call, apply і bind, які дозволяють вказати контекст виклику функції.
+// Вивчимо сигнатуру цього методу. Сигнатура — це ім'я методу і його параметри.
+// Сигнатура методу call**()** виглядає так:
 
-// Викликаємо в контексті об'єкта
-user.showContext(); // this in showThis: {username: "Poly", showContext: ƒ}
+//foo.call(thisArg, arg1, arg2, ...);
+// thisArg — об'єкт, який ми хочемо встановити як контекст (значення this) для функції
+// arg1, arg2, ... — необов'язкові аргументи, які будуть передані функції
 
-// Викликаємо в глобальному контексті
-showThis(); // "this in showThis: undefined"
+//Метод call викликає функцію foo так, що значення this у функції буде посилатися на об'єкт thisArg, і також передає їй аргументи arg1, arg2 тощо.
+// Розглянемо приклад: давай створимо функцію greet, яка вітає різних користувачів готелю, кожен з яких представлений об'єктом з властивостями username і room. Функція приймає один параметр, str — рядок привітання.
 
+{
+  //Розглянемо приклад: давай створимо функцію greet, яка вітає різних користувачів готелю, кожен з яких представлений об'єктом з властивостями username і room. Функція приймає один параметр, str — рядок привітання.
+
+  function greet(str) {
+    console.log(`${str}, ${this.username}, your room is ${this.room}!`);
+  }
+
+  const mango = {
+    username: "Mango",
+    room: 27,
+  };
+
+  const poly = {
+    username: "Poly",
+    room: 191,
+  };
+
+  //За допомогою методу call ми можемо викликати функцію greet так, щоб значення this вказувало на потрібний об'єкт і використовувало значення його властивостей.
+
+  greet.call(mango, "Welcome"); // "Welcome, Mango, your room is 27!"
+  greet.call(poly, "Aloha"); // "Aloha, Poly, your room is 191!"
+}
+//#endregion
+
+//#region Метод apply()
+// Метод apply є аналогом методу call. Відмінність у тому, що в методі apply() синтаксис передачі аргументів вимагає масиву, навіть якщо аргументи функції — це окремі значення.
+
+// Сигнатура методу apply**()** виглядає так:
+// foo.apply(thisArg, [arg1, arg2, ...])
+// thisArg — об'єкт, який ми хочемо встановити як контекст (значення this) для функції.
+// arg1, arg2, ... — необов'язкові аргументи, які будуть передані функції.
+
+// Метод apply викликає функцію foo таким чином, що значення this у функції буде посилатися на об'єкт obj і передасть елементи масиву як окремі аргументи arg1, arg2 тощо.
+
+{
+  // Розгляньмо приклад, де використаємо раніше оголошену нами функцію greet. Цього разу викликаємо її, використовуючи метод apply замість call.
+
+  function greet(str) {
+    console.log(`${str}, ${this.username}, your room is ${this.room}!`);
+  }
+
+  const mango = {
+    username: "Mango",
+    room: 27,
+  };
+
+  const poly = {
+    username: "Poly",
+    room: 191,
+  };
+
+  greet.apply(mango, ["Welcome"]); // "Welcome, Mango, your room is 27!"
+  greet.apply(poly, ["Aloha"]); // "Aloha, Poly, your room is 191!"
+
+  //Тобто різниця між call і apply полягає лише у формі передачі аргументів. Вибір між call і apply залежить від того, як саме ти хочеш передавати аргументи до функції в конкретній ситуації. У всіх інших аспектах вони працюють ідентично: викликають функцію в контексті зазначеного об'єкта і передають їй аргументи.
+}
+//#endregion
+
+//#regionМетод bind() і втрата контексту
+// Методи call і apply викликають функцію «на місці», тобто одразу.
+// Метод bind створює і повертає нову функцію, яка має заздалегідь встановлений контекст, і ця нова функція може бути викликана пізніше з будь-якими аргументами.
+// Сигнатура методу bind() виглядає так:
+// const boundFoo = foo.bind(thisArg, arg1, arg2, ...)
+//thisArg — об'єкт, який ми хочемо встановити як контекст (значення this) для функції
+//arg1, arg2, ... — необов'язкові аргументи, які будуть передані функції при її виклику
+{
+  //Давай спробуємо зберегти посилання на метод об'єкта у змінну і викликати його в глобальному контексті.
+  {
+    ("use strict");
+
+    const customer = {
+      username: "Jacob",
+      sayHello() {
+        console.log(`Hello, ${this.username}!`);
+      },
+    };
+
+    customer.sayHello(); // "Hello, Jacob!"
+
+    const greet = customer.sayHello;
+
+    greet(); // TypeError: Cannot read properties of undefined (reading 'username')
+
+    //Зберегли посилання на метод sayHello у змінну greet
+    // При виклику greet() контекст втрачено, функція викликається в глобальному контексті, тому значення this буде undefined
+    //При спробі звернутися до властивості this.username під час виклику функції greet(), виникає помилка, оскільки undefined — це навіть не об'єкт
+  }
+
+  {
+    // Це можна виправити, прив'язавши контекст за допомогою методу bind.
+
+    ("use strict");
+
+    const customer = {
+      username: "Jacob",
+      sayHello() {
+        console.log(`Hello, ${this.username}!`);
+      },
+    };
+
+    customer.sayHello(); // "Hello, Jacob!"
+
+    const greet = customer.sayHello.bind(customer);
+
+    greet(); // "Hello, Jacob!"
+  }
+
+  //Коли ми використовуємо bind(), ми створюємо нову функцію greet. Ця нова функція завжди матиме правильний контекст і може використовувати властивість username об'єкта customer.
+}
+//#endregion
+
+//#regionМетод bind() і колбеки
+// Метод bind() є дуже корисним. Особливо важливо його використовувати в колбек-функціях і ситуаціях, де можлива втрата контексту під час виклику функції.
+// Розглянемо приклад втрати контексту, коли ми передаємо метод об'єкта як колбек-функцію:
+{
+  ("use strict");
+
+  const customer = {
+    firstName: "Jacob",
+    lastName: "Mercer",
+    getFullName() {
+      return `${this.firstName} ${this.lastName}`;
+    },
+  };
+
+  function makeMessage(callback) {
+    // callback() — це виклик методу getFullName в глобальному контексті
+    const username = callback();
+    console.log(`Processing an application from ${username}`);
+  }
+
+  makeMessage(customer.getFullName); // TypeError: Cannot read properties of undefined (reading 'firstName')
+
+  //У цьому випадку метод getFullName передається як колбек-функція callback і втрачає контекст об'єкта customer. Це призводить до помилки, оскільки значення this у методі getFullName, коли він викликається як колбек у глобальному контексті, є undefined. Звертаючись до властивостей firstName і lastName, ми отримаємо помилку, оскільки undefined — це не об'єкт.
+}
+{
+  // Щоб уникнути цієї втрати контексту, можна використати метод bind(). Замість передачі оригінального методу getFullName, ми передаємо його копію, до якої прив'язаний контекст об'єкта customer.
+
+  const customer = {
+    firstName: "Jacob",
+    lastName: "Mercer",
+    getFullName() {
+      return `${this.firstName} ${this.lastName}`;
+    },
+  };
+
+  function makeMessage(callback) {
+    const username = callback();
+    console.log(`Processing an application from ${username}`);
+  }
+
+  makeMessage(customer.getFullName.bind(customer)); // "Processing an application from Jacob Mercer"
+}
+
+//Example
+{
+  //without bind
+  {
+    ("use strict");
+
+    const library = {
+      books: 1923,
+      logBookCount() {
+        console.log(this.books);
+      },
+    };
+
+    function showBooks(callback) {
+      callback();
+    }
+
+    showBooks(library.logBookCount);
+  }
+  {
+    //with bind
+    ("use strict");
+
+    const library = {
+      books: 1923,
+      logBookCount() {
+        console.log(this.books);
+      },
+    };
+
+    function showBooks(callback) {
+      callback();
+    }
+
+    showBooks(library.logBookCount.bind(library));
+  }
+}
+
+//#endregion
+
+//#region Стрілочні функції
+// Контекст усередині стрілочної функції визначається місцем її оголошення, а не виклику.
+// Це означає, що this усередині стрілки посилається на контекст батьківської області видимості, в якій вона була оголошена, і ніколи не змінюється.
+{
+  const showThis = () => {
+    console.log("this in showThis: ", this);
+  };
+
+  showThis(); // this in showThis: window
+}
+
+//Навіть якщо присвоїти посилання на стрілочну функцію у властивість об'єкта і викликати її в контексті цього об'єкта, this усе одно буде посилатися на об'єкт, який функція запам'ятала в момент її оголошення.
+{
+  const showThis = () => {
+    console.log("this in showThis: ", this);
+  };
+
+  const user = {
+    username: "Mango",
+  };
+
+  user.showContext = showThis;
+
+  user.showContext(); // this in showThis: window
+}
+
+//Стрілочні функції також ігнорують наявність суворого режиму.
+//Якщо стрілка була оголошена в глобальному контексті, то this у ній буде містити посилання на window, незалежно від того, чи виконується скрипт у суворому режимі.´
+{
+  ("use strict");
+
+  const showThis = () => {
+    console.log("this in showThis: ", this);
+  };
+
+  showThis(); // this in showThis: window
+}
+
+//Розгляньмо приклад, який добре ілюструє, як працює контекст для стрілочних функцій.
+{
+  const hotel = {
+    username: "Resort hotel",
+    showThis() {
+      const foo = () => {
+        console.log("this in foo: ", this);
+      };
+
+      foo();
+      console.log("this in showThis: ", this);
+    },
+  };
+
+  hotel.showThis();
+  // this in foo: {username: 'Resort hotel', showThis: ƒ}
+  // this in showThis: {username: 'Resort hotel',showThis: ƒ}
+}
+//Можливо, це не часто використовується на практиці. Але такі кейси досить типові для співбесіди.
+
+{
+  //На відміну від звичайних функцій, змінити значення this усередині стрілки після її оголошення неможливо. Це означає, що методи call, apply і bind не впливають на значення this у стрілках.
+
+  const showThis = () => {
+    console.log("this in showThis: ", this);
+  };
+
+  showThis.call({ username: "Mango" }); // this in showThis: window
+  showThis.apply({ username: "Mango" }); // this in showThis: window
+
+  const boundShowThis = showThis.bind({ username: "Mango" });
+  boundShowThis(); // this in showThis: window
+}
+//Обмежуючи стрілочні функції постійним контекстом, JavaScript-рушії можуть краще їх оптимізувати, на відміну від звичайних функцій, значення this яких може бути змінено.
+
+//#endregion
+
+//#region !!!
+
+//Що треба запам’ятати про this у стрілочних функціях?
+//1.Контекст **this** усередині стрілочної функції визначається місцем її оголошення, а не виклику.
+//2.Стрілочні функції ігнорують наявність суворого режиму. Тому в глобальному контексті у стрілці завжди this посилається на об'єкт window.
+//3.Неможливо змінити значення this усередині стрілочних функцій після її оголошення. Методи call, apply і bind не впливають на значення this у стрілках.
+
+//Алгоритм визначення this
+// Ключове слово this — це одна з найзаплутаніших концепцій для новачка.
+// Новачки часто підставляють this методом наукового тику доти, доки скрипт не спрацює.
+
+//Але все стає значно простішим, коли є простий алгоритм визначення значення this.
+{
+  // Крок 1
+  //Це стрілочна функція?
+  //Якщо відповідь Так, значення this те саме, що у this у зовнішній області видимості
+  //Якщо відповідь Ні, переходь на Крок 2
+}
+{
+  // Крок 2
+  // Чи використовуються методи call, apply або bind?
+  //Якщо відповідь Так, значення this — це той самий об’єкт, що передали при їх виклику
+  //Якщо відповідь Ні, переходь на Крок 3
+}
+{
+  //Крок 3
+  //Функція викликається як метод об’єкта object.method?
+  //Якщо відповідь Так, значення this — це об’єкт ліворуч від крапки
+  //Якщо відповідь Ні, переходь на Крок 4
+}
+{
+  //Крок 4
+  //Скрипт виконується в суворому режимі?
+  //Якщо відповідь Так, значення this — undefined
+  //Якщо відповідь Ні, значення this — window
+}
 //#endregion
