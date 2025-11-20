05244196579db2613b4d59b413c44920a298edd7
oleksii ponomarenko <alexponomarenko996@gmail.com>
2025-11-20 16:53:55 +0100

    update code

--- js/konspekt_goit_js/module_9_(Code modularity and the Vite bundler)/module_9.2_web_storage.js
+++ js/konspekt_goit_js/module_9_(Code modularity and the Vite bundler)/module_9.2_web_storage.js
@@ -57,5 +57,200 @@ console.log(localStorage);
 
 //#region Додавання даних
 //Давай додамо пару ключ-значення до локального сховища за допомогою методу setItem(key, value), доступного в об’єкті localStorage:
+{
+  localStorage.setItem("ui-theme", "light");
+  console.log(localStorage); // Storage {ui-theme: "light", length: 1}
+}
+//А що як у сховищі вже існував запис із ключем "ui-theme"?
+// Виклик методу setItem(key, value) перезапише його значення.
+//#endregion
+
+//#region Додавання складних даних
+
+// У вебсховище не записують методи об'єктів або функції, тільки дані, які підтримує JSON формат.
+// Технічно у вебсховище можна записати тільки рядки. Але це не проблема, якщо використовувати методи класу JSON для перетворення складних типів.
+// Якщо необхідно зберегти щось, окрім рядка, наприклад, масив або об'єкт, необхідно перетворити їх у рядок методом JSON.stringify().
+{
+  const settings = {
+    theme: "dark",
+    isAuthenticated: true,
+    options: [1, 2, 3],
+  };
+
+  localStorage.setItem("settings", JSON.stringify(settings));
+}
+//#endregion
+
+//#region Отримання даних
+
+// Метод getItem(key) дозволяє зчитати зі сховища запис із ключем key і повертає його значення у JSON форматі.
+{
+  const savedTheme = localStorage.getItem("ui-theme");
+  console.log(savedTheme); // "light"
+}
+
+//Якщо у сховищі відсутній запис з таким ключем, метод повертає null.
+{
+  const savedItem = localStorage.getItem("key-that-does-not-exist");
+  console.log(savedItem); // null
+}
+
+//Якщо значення є примітивним типом, немає потреби його парсити.
+// В іншому випадку, якщо це масив або об'єкт, необхідно розпарсити значення методом JSON.parse(), щоб отримати валідні дані.
+{
+  const settings = {
+    theme: "dark",
+    isAuthenticated: true,
+    options: [1, 2, 3],
+  };
+  localStorage.setItem("settings", JSON.stringify(settings));
+
+  const savedSettings = localStorage.getItem("settings");
+  console.log(savedSettings); // A string
+
+  const parsedSettings = JSON.parse(savedSettings);
+  console.log(parsedSettings); // Settings object
+}
+//У змінній savedSettings буде рядок, що представляє об'єкт, тому ми розпарсюємо це значення, і у змінній parsedSettings отримуємо повноцінний об'єкт із властивостями.
+//#endregion
+
+//#region Видалення даних
+// Метод removeItem(key) видаляє зі сховища існуючий запис з ключем key. В результаті своєї роботи він не повертає значення.
+{
+  localStorage.setItem("ui-theme", "dark");
+  console.log(localStorage.getItem("ui-theme")); // "dark"
+
+  localStorage.removeItem("ui-theme");
+  console.log(localStorage.getItem("ui-theme")); // null
+}
+
+//Якщо ти хочеш повністю очистити сховище, потрібно викликати метод clear().
+{
+  localStorage.setItem("ui-theme", "light");
+  localStorage.setItem("notif-level", "mute");
+
+  console.log(localStorage);
+  // Storage {notif-level: 'mute', ui-theme: 'light', length: 2}
+
+  localStorage.clear();
+  console.log(localStorage); // Storage {length: 0}
+}
+//Операція повного очищення сховища є ризикованою. Вона може порушити записи, створені іншими розробниками проєкту. Краще видаляти лише ті записи, які дійсно не потрібні, не покладаючись на повну очистку даних сховища.
+//#endregion
+
+//#region Сховище сесії
+// Сховище сесії зберігає дані лише доти, поки відкрита вкладка браузера.
+// Це означає, що кожного разу, коли відкривається нова вкладка або нове вікно браузера, створюється нове сховище сесії. Отже, будь-які дані, які зберігаються в сховищі сесії, автоматично видаляються, коли користувач закриває цю вкладку/вікно.
+
+// Набір методів та їхній функціонал ідентичні методам роботи з локальним сховищем.
+// Єдиний виняток — звертаємося до них через об'єкт sessionStorage, а не localStorage.
+
+console.log(window.sessionStorage); // Storage {length: 0}
+
+//Методом setItem(key, value) можна записувати як рядки, так і складні типи даних.
+{
+  sessionStorage.setItem("user-id", "123");
+  sessionStorage.setItem(
+    "tickets",
+    JSON.stringify({ from: "Lviv", to: "Kyiv", quantity: 2 })
+  );
+  console.log(sessionStorage);
+  // Storage {user-id: '123', tickets: '{"from":"Lviv","to":"Kyiv","quantity":2}', length: 2}
+}
+
+//Методом getItem(key) можна читати записи, використовуючи збережений ключ.
+{
+  const userId = sessionStorage.getItem("user-id");
+  console.log(userId); // "123"
+
+  const tickets = JSON.parse(sessionStorage.getItem("tickets"));
+  console.log(tickets); // { from: "Lviv", to: "Kyiv", quantity: 2 }
+}
+
+//І звісно, видаляти елементи за ключем і очищати сховище цілком методами removeItem(key) і clear() відповідно.
+{
+  sessionStorage.removeItem("tickets");
+  console.log(sessionStorage); // Storage {user-id: '123', length: 1}
+
+  sessionStorage.clear();
+  console.log(sessionStorage); // Storage {length: 0}
+}
+//#endregion
+
+//#region Кейс: Форма з повідомленням
+// Створимо форму для введення повідомлення.
+//Під час сабміту форми будемо виводити в консоль значення текстового поля та очищати форму.
+{
+  const form = document.querySelector(".feedback-form");
+
+  form.addEventListener("submit", (evt) => {
+    evt.preventDefault();
+    console.log(evt.target.elements.message.value);
+    form.reset();
+  });
+}
+
+//Проблема
+// Якщо користувач ввів повідомлення в текстове поле і перезавантажив сторінку, не надіславши форму, під час перезавантаження сторінки введене повідомлення пропадає.
+
+//Рішення
+// Зробимо так, щоб при перезавантаженні сторінки зберігалося введене повідомлення.
+// Для цього використовуємо локальне сховище, щоб зберегти поточне значення текстового поля під час введення.
+{
+  const form = document.querySelector(".feedback-form");
+  const localStorageKey = "goit-example-message";
+
+  form.addEventListener("input", (evt) => {
+    localStorage.setItem(localStorageKey, evt.target.value);
+  });
+
+  form.addEventListener("submit", (evt) => {
+    evt.preventDefault();
+    console.log(evt.target.elements.message.value);
+    form.reset();
+  });
+}
+
+//Щоразу, коли змінюється значення поля, тобто відбувається подія "input", ми:
+// використовуємо делегування подій;
+// ловимо подію на формі;
+// використовуємо властивість target для запису поточного значення поля в локальне сховище.
+
+//Під час сабміту форми будемо очищати збережене значення методом removeItem.
+{
+  const form = document.querySelector(".feedback-form");
+  const localStorageKey = "goit-example-message";
+
+  form.addEventListener("input", (evt) => {
+    localStorage.setItem(localStorageKey, evt.target.value);
+  });
+
+  form.addEventListener("submit", (evt) => {
+    evt.preventDefault();
+    console.log(evt.target.elements.message.value);
+    localStorage.removeItem(localStorageKey);
+    form.reset();
+  });
+}
+
+//Останнім кроком необхідно додати код читання збереженого повідомлення з локального сховища і встановлення його початковим значенням для текстового поля під час завантаження сторінки.
+{
+  const form = document.querySelector(".feedback-form");
+  const textarea = form.elements.message;
+  const localStorageKey = "goit-example-message";
+
+  textarea.value = localStorage.getItem(localStorageKey) ?? "";
+
+  form.addEventListener("input", (evt) => {
+    localStorage.setItem(localStorageKey, evt.target.value);
+  });
+
+  form.addEventListener("submit", (evt) => {
+    evt.preventDefault();
+    console.log(evt.target.elements.message.value);
+    localStorage.removeItem(localStorageKey);
+    form.reset();
+  });
+}
 
 //#endregion
